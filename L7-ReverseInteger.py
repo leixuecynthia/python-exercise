e3fec4f51cd838c3c673932d9d64bda1d5fa4568
Charlotte99 <leixue2017@outlook.com>
2021-04-24 16:25:59 -0700

    test

---
+++ L7-ReverseInteger.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        return 14
+
+
+
+# ----- run locally ---------
+if __name__ == "__main__":
+    s = Solution()
+    result = s.reverse(10)
+    print(result)
