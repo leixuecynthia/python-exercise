768eeda6482ad563c646ba02a79cb463c15aa04d
Charlotte99 <leixue2017@outlook.com>
2021-05-22 23:12:13 -0700

    sql

---
+++ L1251.sql
@@ -0,0 +1,6 @@
+SELECT Prices.product_id, ROUND(SUM(price*units)/SUM(units),2) AS average_price
+FROM Prices
+JOIN UnitsSold
+ON Prices.product_id = UnitsSold.product_id
+AND (UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date)
+GROUP BY product_id
