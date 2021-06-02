b84d0e07d362dda50de297c046d131925d62c587
Charlotte99 <leixue2017@outlook.com>
2021-06-01 22:28:09 -0700

    sql

---
+++ L1495.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT c.title
+FROM TVProgram p
+    JOIN Content c ON c.content_id = p.content_id
+WHERE c.kids_content = 'Y'
+    AND c.content_type = 'Movies'
+    AND YEAR(p.program_date) = 2020
+    AND MONTH(p.program_date) = 6
